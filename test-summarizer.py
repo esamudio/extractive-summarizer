652ce1e071b3d91d418d24e3b2c4cda18d62e1d8
Edgar Samudio <edgar.samudiob@gmail.com>
2017-05-03 03:01:44 -0400

    added check for name of the scope in which top-level code executes + made files executable

--- test-summarizer.py
+++ test-summarizer.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python3
+
 import argparse
 from utils import load_file
 
 # constants
 DESC = '''This application summarizes a given article using previously calculated inverse document frequencies (idf).'''
-
 IDF_FILE_DESC = '''File containing idf values of words seen in training.'''
-
 ORIGINAL_FILE_DESC = '''Name of file to summarize.'''
 
-# setup argument parser
-parser = argparse.ArgumentParser(prog='test-summarizer', description=DESC)
-parser.add_argument('IDF_FILE', type=str, help=IDF_FILE_DESC)
-parser.add_argument('ORIGINAL_FILE', type=str, help=ORIGINAL_FILE_DESC)
-args = parser.parse_args()
 
-words_idf = load_file(args.IDF_FILE)
+if __name__ == '__main__':
+    # setup argument parser
+    parser = argparse.ArgumentParser(prog='test-summarizer', description=DESC)
+    parser.add_argument('IDF_FILE', type=str, help=IDF_FILE_DESC)
+    parser.add_argument('ORIGINAL_FILE', type=str, help=ORIGINAL_FILE_DESC)
+    args = parser.parse_args()
+
+    words_idf = load_file(args.IDF_FILE)
